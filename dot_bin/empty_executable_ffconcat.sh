b98ccddaa6f8f552b07efc19a7c5475ceb00bcf6
Meow <me@itsmeow.cat>
2025-03-20 16:00:52 -0400

    Update .bin/ffconcat.sh

--- dot_bin/empty_executable_ffconcat.sh
+++ dot_bin/empty_executable_ffconcat.sh
@@ -8,6 +8,14 @@ fi
 output_file="$1"
 shift
 
-printf "%s\n" "$@" | sed "s/^/file '/;s/\$/\'/" | ffmpeg -f concat -safe 0 -i pipe:0 "$output_file"
+temp_list=$(mktemp)
+
+for input in "$@"; do
+    echo "file '$input'" >> "$temp_list"
+done
+
+ffmpeg -f concat -safe 0 -i "$temp_list" "$output_file"
+
+rm -f "$temp_list" 
 
 echo "Videos combined successfully into $output_file"
