d829dcf7a7fe4760f1022da1f674b76da6775928
Meow <me@itsmeow.cat>
2025-11-14 14:10:57 -0500

    oh

--- config/default.nix
+++ config/default.nix
@@ -62,13 +62,12 @@ in
 
     git = {
       enable = true;
-      userName = "Meow";
-      userEmail = "github@xela.codes";
-      #user = {
-      #  name = "Meow";
-      #  email = "github@xela.codes";
-      #};
-      extraConfig = {
+      settings = {
+        user = {
+          name = "Meow";
+          email = "github@xela.codes";
+        };
+      
         pull.rebase = false;
         
         # borrowed from https://github.com/bobvanderlinden/nixos-config/blob/0c09c5c162413816d3278c406d85c05f0010527c/home/default.nix#L938
