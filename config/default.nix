ed9e4e55b1a7f93361b0fa7e3231e94ac3f94626
Meow <github@xela.codes>
2025-11-14 14:13:35 -0500

    format

--- config/default.nix
+++ config/default.nix
@@ -67,9 +67,9 @@ in
           name = "Meow";
           email = "github@xela.codes";
         };
-      
+
         pull.rebase = false;
-        
+
         # borrowed from https://github.com/bobvanderlinden/nixos-config/blob/0c09c5c162413816d3278c406d85c05f0010527c/home/default.nix#L938
         url."git@github.com:".insteadOf = [
           "https://github.com/"
