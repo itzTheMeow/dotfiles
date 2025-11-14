583e5d9a4f7729b4dd50f47b8f6a42230e65878c
Meow <me@itsmeow.cat>
2025-11-14 14:03:49 -0500

    use unstable

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Home Manager config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
