7ab930bc7ec0c9e52212bd0ea2cf6151d20ac92f
Meow <github@xela.codes>
2025-11-14 06:01:55 -0500

    Remove .bin/git-clear.sh Update .bin/init.sh Update .profile_extra Remove .gitconfig

--- dot_bin/executable_init.sh
+++ dot_bin/executable_init.sh
@@ -11,21 +11,18 @@ cd ~
 
 PROMPT=(whiptail --separate-output --checklist "Choose packages to install" 20 78 10)
 OPTIONS=(
-  brew "Homebrew" ON
-  deno "Deno" ON
-  ncdu "NCurses Disk Usage" ON
-  ntfy "NTFY CLI" ON
-  nvm "Node Version Manager" ON
-  omp "Oh My Posh" ON
-  opcli "1Password CLI" ON
-  restic "Restic" ON
-  speedtest "Speedtest CLI" ON
+	brew "Homebrew" ON
+	deno "Deno" ON
+	ntfy "NTFY CLI" ON
+	nvm "Node Version Manager" ON
+	omp "Oh My Posh" ON
+	opcli "1Password CLI" ON
 )
 
 CHOICES=$("${PROMPT[@]}" "${OPTIONS[@]}" 2>&1 >/dev/tty)
 
 if [ -z "$CHOICES" ]; then
-  echo "No extras selected!"
+	echo "No extras selected!"
 fi
 
 # Update/Install Packages
@@ -38,122 +35,81 @@ NCDU_VERSION="${NCDU_VERSION:-2.8}"
 RESTIC_VERSION="${RESTIC_VERSION:-0.18.0}"
 
 inst_brew() {
-  # Install Homebrew
-  echo "Installing Homebrew..."
-  (
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  )
-  # Has to be run outside of shell context.
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+	# Install Homebrew
+	echo "Installing Homebrew..."
+	(
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	)
+	# Has to be run outside of shell context.
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 }
 inst_deno() {
-  # Install Deno
-  echo "Installing Deno..."
-  (
-    curl -fsSL https://deno.land/install.sh | sh
-  )
-}
-inst_ncdu() {
-  # Install NCDU
-  echo "Installing ncdu v$NCDU_VERSION..."
-  (
-    DIR="$(mktemp -d)"
-    cd "$DIR"
-    wget -O ncdu.tar.gz "https://dev.yorhel.nl/download/ncdu-${NCDU_VERSION}-linux-x86_64.tar.gz"
-    tar -xf ncdu.tar.gz
-    chmod +x ncdu
-    sudo mv ncdu /usr/local/bin
-    rm -rf "$DIR"
-  )
+	# Install Deno
+	echo "Installing Deno..."
+	(
+		curl -fsSL https://deno.land/install.sh | sh
+	)
 }
 inst_omp() {
-  # Install Oh My Posh
-  echo "Installing OMP..."
-  (
-    curl -s https://ohmyposh.dev/install.sh | bash -s
-  )
-}
-inst_restic() {
-  # Install Restic
-  echo "Installing restic v$RESTIC_VERSION..."
-  (
-    DIR="$(mktemp -d)"
-    cd "$DIR"
-    wget -O restic.bz2 "https://github.com/restic/restic/releases/download/v${RESTIC_VERSION}/restic_${RESTIC_VERSION}_linux_amd64.bz2"
-    bunzip2 restic.bz2
-    chmod +x restic
-    sudo mv restic /usr/local/bin
-    rm -rf "$DIR"
-  )
+	# Install Oh My Posh
+	echo "Installing OMP..."
+	(
+		curl -s https://ohmyposh.dev/install.sh | bash -s
+	)
 }
 inst_ntfy() {
-  # Install ntfy CLI
-  echo "Installing ntfy CLI"
-  (
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://archive.heckel.io/apt/pubkey.txt | sudo gpg --dearmor -o /etc/apt/keyrings/archive.heckel.io.gpg
-    sudo apt install apt-transport-https
-    sudo sh -c "echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/archive.heckel.io.gpg] https://archive.heckel.io/apt debian main' \
+	# Install ntfy CLI
+	echo "Installing ntfy CLI"
+	(
+		sudo mkdir -p /etc/apt/keyrings
+		curl -fsSL https://archive.heckel.io/apt/pubkey.txt | sudo gpg --dearmor -o /etc/apt/keyrings/archive.heckel.io.gpg
+		sudo apt install apt-transport-https
+		sudo sh -c "echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/archive.heckel.io.gpg] https://archive.heckel.io/apt debian main' \
       > /etc/apt/sources.list.d/archive.heckel.io.list"
-    sudo apt update
-    sudo apt -y install ntfy
-  )
+		sudo apt update
+		sudo apt -y install ntfy
+	)
 }
 inst_nvm() {
-  # Install NVM
-  echo "Installing nvm..."
-  (
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-    echo "Don't forget to install a version of node: nvm install [...]"
-  )
+	# Install NVM
+	echo "Installing nvm..."
+	(
+		curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+		echo "Don't forget to install a version of node: nvm install [...]"
+	)
 }
 inst_onepassword() {
-  # Install 1Password CLI
-  echo "Installing 1Password CLI..."
-  (
-    curl -sS https://downloads.1password.com/linux/keys/1password.asc |
-      sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg &&
-      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
-      sudo tee /etc/apt/sources.list.d/1password.list &&
-      sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/ &&
-      curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol |
-      sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol &&
-      sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22 &&
-      curl -sS https://downloads.1password.com/linux/keys/1password.asc |
-      sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg &&
-      sudo apt update && sudo apt-get -y install 1password-cli
-    echo "Version $(op --version) installed."
-  )
-}
-inst_speedtest() {
-  # Install Speedtest CLI
-  echo "Installing Speedtest CLI..."
-  (
-    curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-    sudo apt-get update
-    sudo apt-get -y install speedtest
-  )
+	# Install 1Password CLI
+	echo "Installing 1Password CLI..."
+	(
+		curl -sS https://downloads.1password.com/linux/keys/1password.asc |
+			sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg &&
+			echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+			sudo tee /etc/apt/sources.list.d/1password.list &&
+			sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/ &&
+			curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol |
+			sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol &&
+			sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22 &&
+			curl -sS https://downloads.1password.com/linux/keys/1password.asc |
+			sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg &&
+			sudo apt update && sudo apt-get -y install 1password-cli
+		echo "Version $(op --version) installed."
+	)
 }
 
 # Install tools.
 for choice in $CHOICES; do
-  if [ "$choice" = "brew" ]; then
-    inst_brew
-  elif [ "$choice" = "deno" ]; then
-    inst_deno
-  elif [ "$choice" = "ncdu" ]; then
-    inst_ncdu
-  elif [ "$choice" = "omp" ]; then
-    inst_omp
-  elif [ "$choice" = "restic" ]; then
-    inst_restic
-  elif [ "$choice" = "ntfy" ]; then
-    inst_ntfy
-  elif [ "$choice" = "nvm" ]; then
-    inst_nvm
-  elif [ "$choice" = "opcli" ]; then
-    inst_onepassword
-  elif [ "$choice" = "speedtest" ]; then
-    inst_speedtest
-  fi
+	if [ "$choice" = "brew" ]; then
+		inst_brew
+	elif [ "$choice" = "deno" ]; then
+		inst_deno
+	elif [ "$choice" = "omp" ]; then
+		inst_omp
+	elif [ "$choice" = "ntfy" ]; then
+		inst_ntfy
+	elif [ "$choice" = "nvm" ]; then
+		inst_nvm
+	elif [ "$choice" = "opcli" ]; then
+		inst_onepassword
+	fi
 done
