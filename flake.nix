da108182c296bc307eae1bd9b23e132dfb85c4db
Meow <github@xela.codes>
2025-11-14 05:39:55 -0500

    add opnix

--- flake.nix
+++ flake.nix
@@ -3,15 +3,21 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.05";
-
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    opnix.url = "github:brizzbuzz/opnix";
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    {
+      nixpkgs,
+      home-manager,
+      opnix,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -21,6 +27,7 @@
         kubuntu = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
+            opnix.homeManagerModules.default
             ./config
             ./config/computers
             ./config/computers/kubuntu.nix
@@ -30,6 +37,7 @@
         hyzenberg = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
+            opnix.homeManagerModules.default
             ./config
             ./config/hyzenberg.nix
           ];
