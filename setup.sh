e2c803d18bd0261b54750963e2050bb4740612ae
Meow <github@xela.codes>
2025-11-18 14:15:46 -0500

    setup script

--- setup.sh
+++ setup.sh
@@ -4,21 +4,18 @@
 
 set -e
 
+echo "Installing nix..."
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
 
+echo "Enabling flakes..."
 mkdir .config/nix
 echo "experimental-features = nix-command flakes" >~/.config/nix/nix.conf
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
-nix-channel --update
-
-nix-shell '<home-manager>' -A install
-
-. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-
+echo "Cloning dotfiles..."
 DOTFILES="$HOME/.dotfiles"
-
 git clone https://github.com/itzTheMeow/dotfiles-nix.git "$DOTFILES"
 cd "$DOTFILES"
 
-echo "Setup complete. Run: home-manager switch --flake .#xxx"
+echo "Setup complete, entering home-manager shell. Run:
+home-manager switch --flake ~/.dotfiles#xxx"
+nix shell nixpkgs#home-manager
