f44746396d1eb7c2259796e9eaa1a82cf3bedd32
Meow <github@xela.codes>
2025-11-18 17:15:08 -0500

    aliases

--- config/common/darwin.nix
+++ config/common/darwin.nix
@@ -6,8 +6,10 @@
 
   home = {
     shellAliases = {
-      # make vscode short command
+      # shortcuts for linux-like shell commands
       code = "open -a /Applications/Visual\ Studio\ Code.app";
+      tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
+
       # remove quarantine status from files
       unquarantine = "xattr -r -d com.apple.quarantine";
     };
