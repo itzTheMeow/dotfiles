89bff0e26468d0f1941074088f5ce218d223e40f
Meow <github@xela.codes>
2025-11-18 18:54:27 -0500

    hide

--- config/common/darwin.nix
+++ config/common/darwin.nix
@@ -5,10 +5,10 @@
 
     ];
 
-    shellAliases = builtins.list {
+    shellAliases = {
       # shortcuts for linux-like shell commands
       code = "open -a /Applications/Visual\ Studio\ Code.app";
-      tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
+      #tailscale = "/Applications/Tailscale.app/Contents/MacOS/Tailscale";
 
       # remove quarantine status from files
       unquarantine = "xattr -r -d com.apple.quarantine";
