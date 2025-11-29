0492ff51169ef8c03d760b4cd34d571125d51bf7
Meow <github@xela.codes>
2025-11-29 17:46:53 -0500

    add name instead

--- config/common/ssh.nix
+++ config/common/ssh.nix
@@ -5,17 +5,17 @@ let
     let
       mkMachine =
         {
+          name,
           host,
-          keyName,
           publicKey,
           extraOptions ? { },
         }:
         {
           inherit host;
-          localPubKeyPath = ".ssh/${keyName}.pub";
+          localPubKeyPath = ".ssh/${host}.pub";
           pubKeyOpPath = publicKey;
           sshOptions = {
-            identityFile = "~/.ssh/${keyName}.pub";
+            identityFile = "~/.ssh/${host}.pub";
             identitiesOnly = true;
           }
           // extraOptions;
@@ -27,7 +27,6 @@ let
       files = builtins.foldl' (
         acc: machine: acc // (utils.mkSecretFile machine.localPubKeyPath machine.pubKeyOpPath)
       ) { } processedMachines;
-
       blocks = builtins.foldl' (
         acc: machine: acc // { "${machine.host}" = machine.sshOptions; }
       ) { } processedMachines;
@@ -35,26 +34,26 @@ let
 in
 mkSSHConfig [
   {
+    name = "Hyzenberg";
     host = "hyzen.xela.codes";
-    keyName = "hyzenberg";
     publicKey = "op://Personal/Hyzenberg SSH Key/public key";
     extraOptions = {
       forwardAgent = true;
     };
   }
   {
+    name = "iPad";
     host = "ipad";
-    keyName = "ipad";
     publicKey = "op://Private/iPad SSH Key/public key";
   }
   {
+    name = "Raspberry PI";
     host = "pi.nvst.ng";
-    keyName = "pi";
     publicKey = "op://NVSTly/Raspberry PI SSH Key/public key";
   }
   {
+    name = "ODROID";
     host = "odroid.nvst.ng";
-    keyName = "odroid";
     publicKey = "op://NVSTly/Odroid SSH Key/public key";
     extraOptions = {
       port = 2222;
@@ -62,24 +61,24 @@ mkSSHConfig [
     };
   }
   {
+    name = "Doris";
     host = "doris.nvst.ng";
-    keyName = "doris";
     publicKey = "op://NVSTly/so7nrwf4gv2uhfsd6qx24gkgz4/public key";
     extraOptions = {
       forwardAgent = true;
     };
   }
   {
+    name = "Jade";
     host = "jade.nvst.ly";
-    keyName = "jade";
     publicKey = "op://NVSTly/Jade SSH Key/public key";
     extraOptions = {
       forwardAgent = true;
     };
   }
   {
+    name = "NetroHost";
     host = "usest1.netro.host";
-    keyName = "netro";
     publicKey = "op://Private/NetroHost SSH/public key";
   }
 ]
