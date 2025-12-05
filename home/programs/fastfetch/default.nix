aeee0a13c2928488943f54d314c7fc624cae0849
Meow <github@xela.codes>
2025-12-05 02:00:39 -0500

    combine modules

--- home/programs/fastfetch/default.nix
+++ home/programs/fastfetch/default.nix
@@ -5,4 +5,6 @@
     settings = builtins.fromJSON (builtins.readFile ./config.json);
   };
   home.file.".config/fastfetch/logo.txt".source = ./logo.txt;
+  home.file.".config/fastfetch/config-title.json".source = ./config-title.json;
+  home.file.".config/fastfetch/config-os.json".source = ./config-os.json;
 }
