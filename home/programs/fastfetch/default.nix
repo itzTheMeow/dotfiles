efcea1545a22ee97896f1213ebeb4610c0107fb1
Meow <github@xela.codes>
2025-12-05 02:33:15 -0500

    accent

--- home/programs/fastfetch/default.nix
+++ home/programs/fastfetch/default.nix
@@ -1,8 +1,21 @@
-{ ... }:
+{ hostname, ... }:
+let
+  accent =
+    if hostname == "kubuntu" then
+      "#cba6f7"
+    else if hostname == "hyzenberg" then
+      "#f38ba8"
+    else if hostname == "macintosh" then
+      "#b4befe"
+    else
+      "blue";
+in
 {
   programs.fastfetch = {
     enable = true;
-    settings = builtins.fromJSON (builtins.readFile ./config.json);
+    settings = builtins.fromJSON (
+      builtins.replaceStrings [ "__ACCENT__" ] [ accent ] (builtins.readFile ./config.json)
+    );
   };
   home.file.".config/fastfetch/logo.txt".source = ./logo.txt;
   home.file.".config/fastfetch/config-title.json".source = ./config-title.json;
