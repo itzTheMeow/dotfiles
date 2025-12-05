868236fccca2db484f0e66b969eacb630c62824d
Meow <github@xela.codes>
2025-12-05 00:42:31 -0500

    Remove .bin/art-nvstly.txt Remove .bin/backblaze.sh Remove .bin/download_organizer.js Remove .bin/ipad_backup.sh Remove .bin/machine-started.sh Remove .bin/restic-migrate.sh Remove .config/activitywatch/aw-client/aw-client.toml Remove .config/activitywatch/aw-qt/aw-qt.toml Remove .config/activitywatch/aw-server-rust/.keep Remove .config/activitywatch/aw-server/aw-server.toml Remove .config/activitywatch/aw-server/settings.json Remove .config/activitywatch/aw-watcher-afk/aw-watcher-afk.toml Remove .config/activitywatch/aw-watcher-window/aw-watcher-window.toml

--- dot_bin/executable_machine-started.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-########################
-#  machine-started.sh  #
-#  by Meow       2025  #
-########################
-# Script to notify the ntfy server that the current machine has started.
-
-ntfy publish "Machine Booted"
