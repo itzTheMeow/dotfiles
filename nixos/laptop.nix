66097052c0f193ea15f395ee99192af77e7e229b
Meow <github@xela.codes>
2025-12-19 02:23:30 -0500

    add 1password

--- nixos/laptop.nix
+++ nixos/laptop.nix
@@ -97,13 +97,10 @@
     nano
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ username ];
+  };
 
   # List services that you want to enable:
 
