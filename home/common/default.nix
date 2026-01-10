e3ec934a8e923c356ca503eb8aad32e15dea173a
Meow <github@xela.codes>
2026-01-10 02:47:33 -0500

    remotes

--- home/common/default.nix
+++ home/common/default.nix
@@ -157,16 +157,28 @@ in
 
     rclone = {
       enable = true;
-      remotes.ipad.config = {
-        type = "sftp";
-        host = "ipad";
-        user = "mobile";
-        key_use_agent = true;
-        pubkey_file = "~/.ssh/ipad.pub";
-        known_hosts_file = "~/.ssh/known_hosts";
-        shell_type = "unix";
-        md5sum_command = "md5sum";
-        sha1sum_command = "sha1sum";
+      remotes = {
+        backblaze.config = {
+          type = "b2";
+          account = "op://Private/Backblaze/Application Keys/xela-codes-nas-id";
+          key = "op://Private/Backblaze/Application Keys/xela-codes-nas-applicationKey";
+        };
+        ipad.config = {
+          type = "sftp";
+          host = "ipad";
+          user = "mobile";
+          key_use_agent = true;
+          pubkey_file = "~/.ssh/ipad.pub";
+          known_hosts_file = "~/.ssh/known_hosts";
+          shell_type = "unix";
+          md5sum_command = "md5sum";
+          sha1sum_command = "sha1sum";
+        };
+        pcloud.config = {
+          type = "pcloud";
+          hostname = "api.pcloud.com";
+          token = "op://Private/pcloud.com/Auth Payload";
+        };
       };
     };
   };
