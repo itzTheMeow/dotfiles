1ec08013611161db968b144d75edffac84edf97e
Meow <github@xela.codes>
2026-01-10 02:28:00 -0500

    fix

--- home/programs/activitywatch/default.nix
+++ home/programs/activitywatch/default.nix
@@ -3,7 +3,6 @@
   services.activitywatch = {
     enable = true;
     watchers = {
-      aw-sync.enable = true;
       aw-watcher-afk.enable = true;
       aw-watcher-input.enable = true;
       aw-watcher-window.enable = true;
