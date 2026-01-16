2f928434332be6aef0b8ee54f67ce514784991a1
Meow <github@xela.codes>
2026-01-16 01:36:26 -0500

    email accounts

--- home/programs/thunderbird/default.nix
+++ home/programs/thunderbird/default.nix
@@ -1,5 +1,40 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  home.packages = [ pkgs.thunderbird ];
+  programs.thunderbird = {
+    enable = true;
+    profiles.default = {
+      isDefault = true;
+    };
+  };
   catppuccin.thunderbird.enable = true;
+
+  accounts = {
+    email = {
+      accounts."alex@xela.codes" = {
+        address = "alex@xela.codes";
+        realName = "Alex";
+        userName = "alex@xela.codes";
+        imap = {
+          host = "mail.xela.codes";
+          port = 993;
+        };
+        smtp = {
+          host = "mail.xela.codes";
+          port = 465;
+        };
+        primary = true;
+        thunderbird.enable = true;
+      };
+    };
+    contact = {
+      accounts.Contacts = {
+        remote = {
+          type = "carddav";
+          url = "https://mail.xela.codes/SOGo/dav/alex@xela.codes/Contacts/personal/";
+          userName = "alex@xela.codes";
+        };
+        thunderbird.enable = true;
+      };
+    };
+  };
 }
