972da8c09b9103fc9223df42761ec1eeab177c88
Meow <github@xela.codes>
2026-01-17 22:42:56 -0500

    make it a nixpkgs package

--- pkgs/colloid-cursors.nix
+++ pkgs/colloid-cursors.nix
@@ -1,30 +1,45 @@
-{ pkgs }:
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+  gitUpdater,
+  variant ? "light", # light or dark
+}:
+let
+  variantString = lib.optionalString (variant == "dark") "-dark";
+in
+assert lib.assertOneOf "variant" variant [
+  "dark"
+  "light"
+];
 
-pkgs.stdenvNoCC.mkDerivation {
+stdenvNoCC.mkDerivation {
   pname = "colloid-cursors";
-  version = "2025-02-09";
+  version = "2025-07-19";
 
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "vinceliuice";
     repo = "Colloid-icon-theme";
-    tag = "2025-02-09";
+    tag = "2025-07-19";
     hash = "sha256-x2SSaIkKm1415avO7R6TPkpghM30HmMdjMFUUyPWZsk=";
   };
 
   installPhase = ''
     runHook preInstall
 
+    # Loosely based on the install script: https://github.com/vinceliuice/Colloid-icon-theme/blob/main/cursors/install.sh
     mkdir -p $out/share/icons
-    cp -r cursors/dist $out/share/icons/Colloid-cursors
-    cp -r cursors/dist-dark $out/share/icons/Colloid-dark-cursors
+    cp -r cursors/dist${variantString} $out/share/icons/Colloid${variantString}-cursors
 
     runHook postInstall
   '';
 
-  meta = with pkgs.lib; {
+  passthru.updateScript = gitUpdater { };
+
+  meta = {
     description = "Colloid cursor theme";
-    homepage = "https://github.com/vinceliuice/Colloid-icon-theme";
-    license = licenses.gpl3Only;
-    platforms = platforms.linux;
+    homepage = "https://github.com/vinceliuice/Colloid-icon-theme/tree/main/cursors#readme";
+    license = lib.licenses.gpl3Only;
+    platforms = lib.platforms.unix;
   };
 }
