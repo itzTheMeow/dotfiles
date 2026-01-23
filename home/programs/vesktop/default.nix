b1c37cd4e9d5df9bf461548b4e376cad5e54d372
Meow <github@xela.codes>
2026-01-23 00:05:53 -0500

    vesktop UA fix

--- home/programs/vesktop/default.nix
+++ home/programs/vesktop/default.nix
@@ -1,5 +1,16 @@
 { pkgs, ... }:
+let
+  vesktop-custom = pkgs.symlinkJoin {
+    name = "vesktop";
+    paths = [ pkgs.vesktop ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/vesktop \
+        --add-flags '--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0"'
+    '';
+  };
+in
 {
-  home.packages = [ pkgs.vesktop ];
+  home.packages = [ vesktop-custom ];
   catppuccin.vesktop.enable = true;
 }
