7c1c3fbdec019f16143183467c51af89c2737bdc
Meow <github@xela.codes>
2026-02-05 18:07:16 -0500

    add user

--- nixos/huell.nix
+++ nixos/huell.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ host, pkgs, ... }:
 {
   imports = [
     ./common
@@ -9,6 +9,15 @@
 
   zramSwap.enable = true;
   networking.hostName = "meow";
+
+  users.users.${host.username} = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAp+ia8qqQVHmHr8fzALeNBse6kBaKGXeWznDN0lAmYE"
