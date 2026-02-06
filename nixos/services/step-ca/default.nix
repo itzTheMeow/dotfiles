ddbcc93e433fe4afa629904cf2be0837f9c8c54a
Meow <github@xela.codes>
2026-02-05 19:13:52 -0500

    settings

--- nixos/services/step-ca/default.nix
+++ nixos/services/step-ca/default.nix
@@ -1,16 +1,57 @@
-{ config, pkgs, xelib, ... }:
+{
+  config,
+  pkgs,
+  xelib,
+  ...
+}:
 let
-  # Use Tailscale IP so other devices on the network can access the CA
   tailscaleIP = xelib.hosts.hyzenberg.ip;
   caPort = xelib.services.step-ca.port;
 in
 {
-  # Local ACME CA for .xela and .internal domains
   services.step-ca = {
     enable = true;
     address = tailscaleIP;
     port = caPort;
     intermediatePasswordFile = "/var/lib/step-ca/password.txt";
+    settings = {
+      root = "/var/lib/step-ca/certs/root_ca.crt";
+      federatedRoots = null;
+      crt = "/var/lib/step-ca/certs/intermediate_ca.crt";
+      key = "/var/lib/step-ca/secrets/intermediate_ca_key";
+      address = "${tailscaleIP}:${toString caPort}";
+      insecureAddress = "";
+      dnsNames = [
+        "localhost"
+        "hyzenberg"
+        tailscaleIP
+      ];
+      logger = {
+        format = "text";
+      };
+      db = {
+        type = "badgerv2";
+        dataSource = "/var/lib/step-ca/db";
+        badgerFileLoadingMode = "";
+      };
+      authority = {
+        provisioners = [
+          {
+            type = "ACME";
+            name = "acme";
+          }
+        ];
+      };
+      tls = {
+        cipherSuites = [
+          "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"
+          "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+        ];
+        minVersion = 1.2;
+        maxVersion = 1.3;
+        renegotiation = false;
+      };
+    };
   };
 
   # Ensure the step-ca service starts on boot
