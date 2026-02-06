51b2862597f112120fd028762bc4a38c0ed98347
Meow <github@xela.codes>
2026-02-05 21:58:20 -0500

    use merge

--- nixos/services/servarr/prowlarr.nix
+++ nixos/services/servarr/prowlarr.nix
@@ -1,31 +1,25 @@
-{ xelib, ... }:
+{ xelib, lib, ... }:
 let
   svc = xelib.services.prowlarr;
   bindaddress = xelib.hosts.${svc.host}.ip;
-
-  proxyConfig =
-    xelib.mkNginxProxy "prowlarr.xela" "http://${xelib.hosts.${svc.host}.ip}:${toString svc.port}"
-      { };
 in
-{
-  services.prowlarr = {
-    enable = true;
-    settings = {
-      app = {
-        instancename = "Prowlarr";
-        theme = "dark";
-      };
-      server = {
-        inherit bindaddress;
-        port = svc.port;
-        urlbase = "/";
+lib.mkMerge [
+  {
+    services.prowlarr = {
+      enable = true;
+      settings = {
+        app = {
+          instancename = "Prowlarr";
+          theme = "dark";
+        };
+        server = {
+          inherit bindaddress;
+          port = svc.port;
+          urlbase = "/";
+        };
       };
     };
-  };
-  systemd.services.prowlarr.after = [ "tailscale-online.service" ];
-
-  security.acme.certs = proxyConfig.security.acme.certs;
-  services.nginx.virtualHosts = proxyConfig.services.nginx.virtualHosts;
-}
-# include the host settings for the domain
-#// xelib.mkNginxProxy "prowlarr.xela" "http://${bindaddress}:${toString svc.port}" { }
+    systemd.services.prowlarr.after = [ "tailscale-online.service" ];
+  }
+  (xelib.mkNginxProxy "prowlarr.xela" "http://${xelib.hosts.${svc.host}.ip}:${toString svc.port}" { })
+]
