450f128fbb9d228c47c579f6c16c7ebb2b0301ff
Meow <github@xela.codes>
2026-02-05 19:52:28 -0500

    comments

--- nixos/common/headless.nix
+++ nixos/common/headless.nix
@@ -1,9 +1,11 @@
 { host, pkgs, ... }:
 {
   systemd.services."home-manager-${host.username}" = {
+    # stops from reactivating on system boot
     wantedBy = pkgs.lib.mkForce [ ];
 
     serviceConfig = {
+      # include saved 1password session in environment
       EnvironmentFile = "-/run/1password-session";
     };
   };
