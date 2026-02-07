d2d007d92430a58226f2d4621712d8b5f74a3d05
Meow <github@xela.codes>
2026-02-07 05:40:38 -0500

    i dont think this is a thing

--- xelib/default.nix
+++ xelib/default.nix
@@ -167,57 +167,77 @@ rec {
       extraConfig ? { },
       proxyWebsockets ? true,
     }:
-    {
-      services.nginx.virtualHosts."${domain}" = lib.mkMerge [
-        {
-          forceSSL = true;
-          useACMEHost = domain;
-          locations."/" = {
-            proxyPass = target;
-            inherit proxyWebsockets;
-          }
-          // (
-            if useLocalCA then
-              {
-                extraConfig = ''
-                  # local domains dont have a body size limit
-                  client_max_body_size 0;
-
-                  # allow Tailscale IP ranges
-                  allow 100.64.0.0/10;
-                  allow fd7a:115c:a1e0::/48;
-
-                  # allow local ips
-                  allow 127.0.0.1;
-                  allow ::1;
-
-                  # block all other traffic
-                  deny all;
-                '';
-              }
-            else
-              { }
-          );
-        }
-        extraConfig
-      ];
-
-      # create cert for this domain
-      security.acme.certs."${domain}" = {
-        email = "ca@xela.codes";
-        webroot = "/var/lib/acme/acme-challenge";
-        group = "nginx";
-      }
-      // (
-        # use the custom ACME server
-        if useLocalCA then
+    lib.mkMerge [
+      {
+        services.nginx.virtualHosts."${domain}" = lib.mkMerge [
           {
-            server = "https://${
-              hosts.${services.step-ca.host}.ip
-            }:${toString services.step-ca.port}/acme/acme/directory";
+            forceSSL = true;
+            useACMEHost = domain;
+            locations."/" = {
+              proxyPass = target;
+              inherit proxyWebsockets;
+            }
+            // (
+              if useLocalCA then
+                {
+                  extraConfig = ''
+                    # local domains dont have a body size limit
+                    client_max_body_size 0;
+
+                    # allow Tailscale IP ranges
+                    allow 100.64.0.0/10;
+                    allow fd7a:115c:a1e0::/48;
+
+                    # allow local ips
+                    allow 127.0.0.1;
+                    allow ::1;
+
+                    # block all other traffic
+                    deny all;
+                  '';
+                }
+              else
+                { }
+            );
           }
-        else
-          { }
-      );
-    };
+          extraConfig
+        ];
+
+        # create cert for this domain
+        security.acme.certs."${domain}" = {
+          email = "ca@xela.codes";
+          webroot = "/var/lib/acme/acme-challenge";
+          group = "nginx";
+        }
+        // (
+          # use the custom ACME server
+          if useLocalCA then
+            {
+              server = "https://${
+                hosts.${services.step-ca.host}.ip
+              }:${toString services.step-ca.port}/acme/acme/directory";
+            }
+          else
+            { }
+        );
+      }
+      # for public domains, add HTTP vhost for ACME challenges
+      (lib.mkIf (!useLocalCA) {
+        services.nginx.virtualHosts."${domain}-acme" = {
+          serverName = domain;
+          listen = [
+            {
+              addr = "0.0.0.0";
+              port = 80;
+            }
+          ];
+          locations."/.well-known/acme-challenge" = {
+            root = "/var/lib/acme/acme-challenge";
+          };
+          locations."/" = {
+            return = "301 https://$host$request_uri";
+          };
+        };
+      })
+    ];
 }
