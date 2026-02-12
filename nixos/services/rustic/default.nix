120a5300a281324cb83018c8f991ea29f56c6407
Meow <github@xela.codes>
2026-02-12 03:14:27 -0500

    fix user config

--- nixos/services/rustic/default.nix
+++ nixos/services/rustic/default.nix
@@ -7,14 +7,25 @@ profileName:
 }:
 {
   # rustic backup scheduler
-  systemd.services.rustic-backup = {
-    description = "Rustic backup";
-    environment = xelib.globals.environment;
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.zsh}/bin/zsh -c 'export PATH=/home/${host.username}/.nix-profile/bin:$PATH && HOME=/home/${host.username} RCLONE_CONFIG=/home/${host.username}/.config/rclone/rclone.conf ${pkgs.rustic}/bin/rustic -P ${profileName} backup'";
+  systemd.services.rustic-backup =
+    let
+      home = /home/${host.username};
+      env = [
+        # define these so rustic will work properly off of the user config
+        "PATH=${home}/.nix-profile/bin:$PATH"
+        "HOME=${home}"
+        "RCLONE_CONFIG=${home}/.config/rclone/rclone.conf"
+        "NTFY_CONFIG=${home}/.config/ntfy/client.yml"
+      ];
+    in
+    {
+      description = "Rustic backup";
+      environment = xelib.globals.environment;
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.zsh}/bin/zsh -c 'export ${builtins.concatStringsSep " " env} && ${pkgs.rustic}/bin/rustic -P ${profileName} backup'";
+      };
     };
-  };
   systemd.timers.rustic-backup = {
     description = "Rustic backup timer";
     wantedBy = [ "timers.target" ];
