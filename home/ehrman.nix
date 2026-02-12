c95d90cd5001a6d80b3cc9648282e163439be4e3
Meow <github@xela.codes>
2026-02-12 02:49:19 -0500

    ntfy secrets

--- home/ehrman.nix
+++ home/ehrman.nix
@@ -20,6 +20,7 @@
       { }
       # secrets
       // xelib.mkSecretFile ".ssh/authorized_keys" "op://Private/vywbzem32jihjjvgldmz5tr5mu/public key"
+      // xelib.mkSecretFile ".config/ntfy/client.yml" "default-token: op://Private/ntfy/Access Tokens/Ehrman"
       # opunattended secrets
       // xelib.mkOPUnattendedSecret "op://Private/6z2tlumg4aiznrno7mnryjunsq/password";
   };
--- home/hyzenberg.nix
+++ home/hyzenberg.nix
@@ -25,6 +25,7 @@
       { }
       # secrets
       // xelib.mkSecretFile ".ssh/authorized_keys" "op://Private/eka63wejfdkiypenxptm6xky54/public key"
+      // xelib.mkSecretFile ".config/ntfy/client.yml" "default-token: op://Private/ntfy/Access Tokens/Hyzenberg"
       # opunattended secrets
       // xelib.mkOPUnattendedSecret "op://Private/6z2tlumg4aiznrno7mnryjunsq/password";
   };
