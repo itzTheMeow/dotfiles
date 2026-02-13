70950f631053c1d6340752355ca412db23680e06
Meow <github@xela.codes>
2026-02-13 03:14:34 -0500

    remove Thunderbird email and contact account configurations

--- home/programs/thunderbird/default.nix
+++ home/programs/thunderbird/default.nix
@@ -7,34 +7,4 @@
     };
   };
   catppuccin.thunderbird.enable = true;
-
-  accounts = {
-    email = {
-      accounts."alex@xela.codes" = {
-        address = "alex@xela.codes";
-        realName = "Alex";
-        userName = "alex@xela.codes";
-        imap = {
-          host = "mail.xela.codes";
-          port = 993;
-        };
-        smtp = {
-          host = "mail.xela.codes";
-          port = 465;
-        };
-        primary = true;
-        thunderbird.enable = true;
-      };
-    };
-    contact = {
-      accounts.Contacts = {
-        remote = {
-          type = "carddav";
-          url = "https://mail.xela.codes/SOGo/dav/alex@xela.codes/Contacts/personal/";
-          userName = "alex@xela.codes";
-        };
-        thunderbird.enable = true;
-      };
-    };
-  };
 }
