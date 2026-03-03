4c02e530b68ce434830e3e591b99c3ff9d69d094
Meow <github@xela.codes>
2026-03-02 20:54:09 -0500

    add images to hk game package

--- pkgs/game-hells-kitchen.nix
+++ pkgs/game-hells-kitchen.nix
@@ -22,9 +22,18 @@ let
     dontUnpack = true;
 
     installPhase = ''
-      # extract the exe and game source
+      # extract the exe, game source, and images
+      7z e ${gameISO} "Icon.ico" -o$out
       7z e ${gameISO} "Main/Files/Hell's Kitchen.exe" -o$out
       7z e ${gameISO} "Main/Files/HellsKitchen.zip" -o$out
+      7z e ${gameISO} "Support/Manual/eng/Manual_files/frontpage.jpg" -o$out
+      mv $out/frontpage.jpg $out/cover.jpg
+      cp ${
+        fetchurl {
+          url = "https://web.archive.org/web/20260303014312/https://screens.16bit.pl/hells-kitchen-the-game/4.jpg";
+          sha256 = "sha256-9dbgsdzloJ0/H+9o9gwsMl7of7HnWRYA3mOqu/w9zuo=";
+        }
+      } $out/background.jpg
     '';
   };
 
@@ -46,15 +55,23 @@ let
     ''}
     ${winebin}server -w
   '';
+
+  launcher = pkgs.writeShellScriptBin "hells-kitchen" ''
+    # put the wine prefix in the data directory
+    export WINEPREFIX="$HOME/.local/share/hells-kitchen"
+
+    # initialize the wine prefix 
+    ${xelpkgs.shared-wine-prefix}/bin/setup ${setupScript}
+
+    # actually launch the game
+    echo "Launching..."
+    ${winebin} "${gameSource}/Hell's Kitchen.exe"
+  '';
 in
-pkgs.writeShellScriptBin "hells-kitchen" ''
-  # put the wine prefix in the data directory
-  export WINEPREFIX="$HOME/.local/share/hells-kitchen"
-
-  # initialize the wine prefix 
-  ${xelpkgs.shared-wine-prefix}/bin/setup ${setupScript}
-
-  # actually launch the game
-  echo "Launching..."
-  ${winebin} "${gameSource}/Hell's Kitchen.exe"
-''
+pkgs.symlinkJoin {
+  name = "game-hells-kitchen";
+  paths = [
+    gameSource
+    launcher
+  ];
+}
