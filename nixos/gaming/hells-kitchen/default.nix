8fcb5824921e770a16d24cc5feba46e087aa54da
Meow <github@xela.codes>
2026-03-08 01:38:46 -0500

    cleanup

--- nixos/gaming/hells-kitchen/default.nix
+++ nixos/gaming/hells-kitchen/default.nix
@@ -13,7 +13,6 @@ in
         # extract the 128x128 layer from the ico
         ''convert "${pkg}/Icon.ico[2]" -background none -gravity center -extent 192x192 "$out"'';
     poster = ./poster.webp;
-    # https://web.archive.org/web/20260303203009if_/https://cdn2.steamgriddb.com/hero_thumb/410525841bbf485b7c29db7db4da9b18.jpg
     screenshot = [
       ./screenshot1.webp
       ./screenshot2.webp
