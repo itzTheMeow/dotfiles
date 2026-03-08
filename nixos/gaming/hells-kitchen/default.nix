4ab5368c05c669c33dfab166aed1e1eb3c9c145f
Meow <github@xela.codes>
2026-03-08 01:37:40 -0500

    move HK package to game dir

--- nixos/gaming/hells-kitchen/default.nix
+++ nixos/gaming/hells-kitchen/default.nix
@@ -1,14 +1,17 @@
 { pkgs, xelpkgs, ... }:
+let
+  pkg = pkgs.callPackage ./pkg.nix { inherit xelpkgs; };
+in
 {
   title = "Hell's Kitchen: The Game";
   collections = [ "PC" ];
-  files = [ "${xelpkgs.game-hells-kitchen}/bin/hells-kitchen" ];
+  files = [ "${pkg}/bin/hells-kitchen" ];
   favorite = true;
   assets = {
     logo =
       pkgs.runCommand "hells-kitchen-logo.png" { nativeBuildInputs = [ pkgs.imagemagick ]; }
         # extract the 128x128 layer from the ico
-        ''convert "${xelpkgs.game-hells-kitchen}/Icon.ico[2]" -background none -gravity center -extent 192x192 "$out"'';
+        ''convert "${pkg}/Icon.ico[2]" -background none -gravity center -extent 192x192 "$out"'';
     poster = ./poster.webp;
     # https://web.archive.org/web/20260303203009if_/https://cdn2.steamgriddb.com/hero_thumb/410525841bbf485b7c29db7db4da9b18.jpg
     screenshot = [
