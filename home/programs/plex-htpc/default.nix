90603d8643e21e3f810814910376b98326f9faa2
Meow <github@xela.codes>
2026-03-11 02:01:18 -0400

    fix htpc

--- home/programs/plex-htpc/default.nix
+++ home/programs/plex-htpc/default.nix
@@ -1,5 +1,17 @@
 { pkgs, xelib, ... }:
 {
-  home.packages = [ (xelib.injectCursorsFHS pkgs.plex-htpc) ];
+  home.packages = [
+    ((xelib.injectCursorsFHS pkgs.plex-htpc).overrideAttrs (old: {
+      # we have to remove the qt6 paths and switch to a more generic theme for it to run under bigscreen
+      nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
+      postFixup = (old.postFixup or "") + ''
+        wrapProgram $out/bin/plex-htpc \
+          --unset QT_QUICK_CONTROLS_STYLE \
+          --unset QT_PLUGIN_PATH \
+          --unset QML2_IMPORT_PATH \
+          --set QT_QPA_PLATFORMTHEME "fusion"
+      '';
+    }))
+  ];
   xdg.dataFile."plex/inputmaps/keyboard.json".source = ./inputmap-keyboard.json;
 }
