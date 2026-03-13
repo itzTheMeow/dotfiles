7c7ba09f0b4869c184d38aa38249cce11ee95fe6
itzTheMeow <50887230+itzTheMeow@users.noreply.github.com>
2026-03-13 19:36:54 +0000

    chore: format code

--- flake.nix
+++ flake.nix
@@ -176,13 +176,17 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = extras // {inherit xelib;};
+              home-manager.extraSpecialArgs = extras // {
+                inherit xelib;
+              };
               home-manager.sharedModules = home-manager-modules;
               home-manager.users.root = import ./home/common;
               home-manager.users.${extras.host.username} = import ./home/${hostname}.nix;
             }
           ];
-          specialArgs = extras // {inherit xelib;};
+          specialArgs = extras // {
+            inherit xelib;
+          };
         };
     in
     {
