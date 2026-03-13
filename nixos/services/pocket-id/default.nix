89a0a3937ef8ef95383325e0d6dad6c11767d627
Meow <github@xela.codes>
2026-03-13 00:19:43 -0400

    use new module

--- nixos/services/pocket-id/default.nix
+++ nixos/services/pocket-id/default.nix
@@ -1,5 +1,6 @@
 {
   config,
+  inputs,
   lib,
   pkgs-unstable,
   xelib,
@@ -8,36 +9,43 @@
 let
   svc = xelib.services.pocket-id;
 in
-lib.mkMerge [
-  {
-    services.pocket-id = {
-      enable = true;
-      package = pkgs-unstable.pocket-id;
-      settings = {
-        HOST = xelib.hosts.${svc.host}.ip;
-        PORT = svc.port;
-        APP_URL = "https://${svc.domain}";
-        TRUST_PROXY = true;
-        #TODO:nix-26.05 switch to credentials option
-        ENCRYPTION_KEY_FILE = config.sops.secrets.pocket-id-enc.path;
-        MAXMIND_LICENSE_KEY_FILE = config.sops.secrets.pocket-id-maxmind.path;
-      };
-    };
+{
+  # replace the module with the one from unstable
+  disabledModules = [ "services/security/pocket-id.nix" ];
+  imports = [ "${inputs.nixpkgs-unstable}/nixos/modules/services/security/pocket-id.nix" ];
 
-    sops.secrets.pocket-id-enc = {
-      sopsFile = ../../../${config.sops.opSecrets.pocket-id.path};
-      key = "key";
-    };
-    sops.secrets.pocket-id-maxmind = {
-      sopsFile = ../../../${config.sops.opSecrets.pocket-id.path};
-      key = "license";
-    };
-    sops.opSecrets = {
-      pocket-id.keys = {
-        key = "op://Private/pwdsgmanpl46sqdbxfsa7ylzzq/credential";
-        license = "op://Private/yo5ksl7xuwir3ab3idjpjccaty/ko4vnnqfnsekir7iss47wdawvq/pzru4hfyoodf34v7uys6cee3ra";
+  config = lib.mkMerge [
+    {
+      services.pocket-id = {
+        enable = true;
+        package = pkgs-unstable.pocket-id;
+        settings = {
+          HOST = xelib.hosts.${svc.host}.ip;
+          PORT = svc.port;
+          APP_URL = "https://${svc.domain}";
+          TRUST_PROXY = true;
+        };
+        credentials = {
+          ENCRYPTION_KEY_FILE = config.sops.secrets.pocket-id-enc.path;
+          MAXMIND_LICENSE_KEY_FILE = config.sops.secrets.pocket-id-maxmind.path;
+        };
       };
-    };
-  }
-  (xelib.mkNginxProxy svc.domain "http://${xelib.hosts.${svc.host}.ip}:${toString svc.port}" { })
-]
+
+      sops.secrets.pocket-id-enc = {
+        sopsFile = ../../../${config.sops.opSecrets.pocket-id.path};
+        key = "key";
+      };
+      sops.secrets.pocket-id-maxmind = {
+        sopsFile = ../../../${config.sops.opSecrets.pocket-id.path};
+        key = "license";
+      };
+      sops.opSecrets = {
+        pocket-id.keys = {
+          key = "op://Private/pwdsgmanpl46sqdbxfsa7ylzzq/credential";
+          license = "op://Private/yo5ksl7xuwir3ab3idjpjccaty/ko4vnnqfnsekir7iss47wdawvq/pzru4hfyoodf34v7uys6cee3ra";
+        };
+      };
+    }
+    (xelib.mkNginxProxy svc.domain "http://${xelib.hosts.${svc.host}.ip}:${toString svc.port}" { })
+  ];
+}
