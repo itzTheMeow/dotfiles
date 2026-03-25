7f2bfd43e8a90837eb7fd68026715e92ce5e3ccb
Meow <github@xela.codes>
2026-03-24 20:34:55 -0400

    useOrigin

--- nixos/hyzenberg/milfmail.net/default.nix
+++ nixos/hyzenberg/milfmail.net/default.nix
@@ -14,7 +14,11 @@ in
     with xelib.dns;
     {
       ${domain} = lib.mkMerge [
-        { inherit SOA NS TTL; }
+
+        {
+          useOrigin = true;
+          inherit SOA NS TTL;
+        }
         (pointHost hostname)
         (mailcow {
           dkimKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqJildZaRmkd3730rfJmTgybr7slKMdqwj20xFh1HDYFN5mEe1Qg6kbKzD+cD0/4Z8ZloIAvonwESA6164pGQyFhG5+dvEqrjc1GdHbQgsygIx7INuY4yqwz0L3/5g4gt2C+wklf6IqLh8v80LMWvJn6Z6TGfMj6K6QwF3NSod91h7e9L0aoGind35zHjDTaMAFtva+Xwgf2OCLDlrynosOR8lsrcCTJpD8z8jgfsykmMOu8eE6JvOzUWg7SJMX2aowTLFaPWS7uo0pJWfuGAWZ/Ru2BmEqk6YLQm/MnbaCTCp7nEqyRQP9ga6uNtkpr3m/ucOWI+Wo0u/ZfRMM4+EQIDAQAB";
