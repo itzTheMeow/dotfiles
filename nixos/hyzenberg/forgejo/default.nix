cc9a543f8c1a3bcb26b47c8f588ca9e29f8a7ef9
Meow <github@xela.codes>
2026-03-25 05:04:52 -0400

    forgejo settings

--- nixos/hyzenberg/forgejo/default.nix
+++ nixos/hyzenberg/forgejo/default.nix
@@ -1,4 +1,9 @@
-{ config, ... }:
+{
+  config,
+  hostname,
+  xelib,
+  ...
+}:
 let
   app = config.apps.forgejo;
 in
@@ -13,14 +18,43 @@ in
     enable = true;
     database.type = "postgres";
     lfs.enable = true;
+    # https://forgejo.org/docs/latest/admin/config-cheat-sheet/
     settings = {
       server = {
         ROOT_URL = app.url;
         DOMAIN = app.domain;
         HTTP_ADDR = app.ip;
         HTTP_PORT = app.port;
+
+        # use internal ssh server on public IP
+        START_SSH_SERVER = true;
+        SSH_LISTEN_HOST = xelib.dns.addr.${hostname};
+
+        LANDING_PAGE = "/xela.codes/"; # redirect unauthenticated users to my account
       };
-      session.COOKIE_SECURE = true;
+      session.COOKIE_SECURE = true; # make cookies secure
+      repository.DEFAULT_BRANCH = "master"; # change default branch name
+      ui.meta.DESCRIPTION = "xela.codes personal software forge.";
+      security.INSTALL_LOCK = true; # disable install page
+      mirror.DEFAULT_INTERVAL = "1h"; # default mirror interval
+
+      # configure oidc
+      openid.ENABLE_OPENID_SIGNUP = true;
+      oauth2_client = {
+        REGISTER_EMAIL_CONFIRM = true;
+        OPENID_CONNECT_SCOPES = "email profile";
+        ENABLE_AUTO_REGISTRATION = true;
+        USERNAME = "preferred_username";
+        UPDATE_AVATAR = true;
+      };
+
+      service = {
+        DISABLE_REGISTRATION = true;
+        ENABLE_INTERNAL_SIGNIN = false;
+        DEFAULT_KEEP_EMAIL_PRIVATE = true;
+        DEFAULT_ORG_MEMBER_VISIBLE = true;
+        NO_REPLY_ADDRESS = "noreply.DOMAIN";
+      };
     };
   };
 }
