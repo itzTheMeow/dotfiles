924e76efd54db0868037c7c5c607b418b24f43c2
Meow <me@itsmeow.cat>
2026-04-10 14:21:06 -0400

    prepare for update

--- nixos/hyzenberg/immich/default.nix
+++ nixos/hyzenberg/immich/default.nix
@@ -21,6 +21,8 @@ in
     package = pkgs-unstable.immich;
     host = app.ip;
     inherit (app) port;
+    # todo:
+    # IMMICH_HELMET_FILE=true
   };
   systemd.services.immich-server.after = [ "tailscale-online.service" ];
 }
