35fbcd6d50798970c4a566ced24e87ab9902d345
Meow <github@xela.codes>
2026-04-26 06:55:49 -0400

    nxr short util

--- home/common/default.nix
+++ home/common/default.nix
@@ -47,6 +47,7 @@ in
       # custom scripts
       (writeShellScriptBin "0x0" ''curl -A "xela.codes/1.0.0" -F "file=@$1" https://0x0.st'')
       (writeShellScriptBin "ffconcat" (builtins.readFile ../../scripts/ffconcat.sh))
+      (writeShellScriptBin "nxr" "nix run .#$1")
       # custom packages
       xelpkgs.download-organizer
       xelpkgs.nx
