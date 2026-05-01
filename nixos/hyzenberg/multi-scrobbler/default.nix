ab570fda411c27d5863eb9c8bb15a0e9a04e1b34
Meow <github@xela.codes>
2026-04-30 23:53:03 -0400

    roll back

--- nixos/hyzenberg/multi-scrobbler/default.nix
+++ nixos/hyzenberg/multi-scrobbler/default.nix
@@ -5,7 +5,7 @@ let
 in
 {
   virtualisation.oci-containers.containers.multi-scrobbler = {
-    image = "docker.io/foxxmd/multi-scrobbler:0.13.1";
+    image = "docker.io/foxxmd/multi-scrobbler:0.10.8";
     autoStart = true;
     ports = [ "${addr}:9078" ];
     volumes = [ "${dataDir}:/config" ];
