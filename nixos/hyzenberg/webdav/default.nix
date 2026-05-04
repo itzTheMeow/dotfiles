1517e008dbb4fbf8eccd5af467d1da2b3f0751ad
Meow <github@xela.codes>
2026-05-04 04:37:31 -0400

    webdav serve

---
+++ nixos/hyzenberg/webdav/default.nix
@@ -0,0 +1,28 @@
+{ config, host, ... }:
+let
+  app = config.apps.webdav;
+in
+{
+  apps.webdav = {
+    domain = "webdav.xela";
+    port = 39834;
+    enableProxy = true;
+  };
+
+  # serve the webdav directory
+  home-manager.users.${host.username}.programs.rclone.remotes.pcloud.serve."/Misc/AppData/webdav" = {
+    enable = true;
+    protocol = "webdav";
+    options = {
+      addr = "${app.ip}:${app.portString}";
+      htpasswd = config.sops.secrets.webdav-htpasswd;
+    };
+  };
+
+  sops.secrets.webdav-htpasswd = {
+    sopsFile = config.sops.opSecrets.webdav.fullPath;
+    key = "htpasswd";
+    owner = host.username;
+  };
+  sops.opSecrets.webdav.keys.htpasswd = "op://Private/zdj2mgqfhx2lue2iehaxgejyzy/htpasswd";
+}
