ab10fccfdbfe21602b161bd8b244453f55d4aed9
Meow <github@xela.codes>
2026-05-04 03:03:35 -0400

    plex media server

--- nixos/hyzenberg/homepage/default.nix
+++ nixos/hyzenberg/homepage/default.nix
@@ -62,6 +62,11 @@ in
           ];
         }
         {
+          Media = [
+            (srv "plex")
+          ];
+        }
+        {
           Downloads = [
             (srv "sonarr")
             (srv "radarr")
---
+++ nixos/hyzenberg/plex/default.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  xelib,
+  ...
+}:
+let
+  app = config.apps.plex;
+  pub = xelib.dns.splitDomain app.details.publicDomain;
+in
+{
+  apps.plex = {
+    domain = "plex.xela";
+    port = 32400;
+    enableProxy = true;
+    details = {
+      publicDomain = "plex.${xelib.domain}";
+    };
+
+    description = "Media Server";
+  };
+
+  # fsr plex doesnt let you change the bind address/port...
+  services.plex.enable = true;
+  systemd.services.plex.after = [ "tailscale-online.service" ];
+
+  # set up public domain too
+  nginx.proxy.${app.details.publicDomain}.target = {
+    host = app.ip;
+    port = app.port;
+  };
+  dnszones.list."${pub.domain}".subdomains."${pub.subdomain}" = xelib.dns.pointHost app.host;
+}
