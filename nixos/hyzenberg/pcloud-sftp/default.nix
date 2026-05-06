5d8f4ab0ee27064fdf728ec285613c54ab8109d9
Meow <github@xela.codes>
2026-05-06 01:17:13 -0400

    own the key

--- nixos/hyzenberg/pcloud-sftp/default.nix
+++ nixos/hyzenberg/pcloud-sftp/default.nix
@@ -1,10 +1,11 @@
-{ config, ... }:
+{ config, host, ... }:
 {
   home-manager.importUser = [ ./rclone.hm.nix ];
 
   sops.secrets.pcloud-sftp-authorizedkeys = {
     sopsFile = config.sops.opSecrets.rclone.fullPath;
     key = "pcloud-sftp-authorizedkeys";
+    owner = host.username;
   };
   sops.opSecrets.rclone.keys.pcloud-sftp-authorizedkeys =
     "op://Private/hl57o5bovxionexdk4cahxzr5y/public key";
