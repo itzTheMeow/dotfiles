a09c4d6c6570180faf3e1292091470c21e608bfa
Meow <github@xela.codes>
2026-06-09 18:05:10 -0400

    idiot

--- nixos/flynn/hardware.nix
+++ nixos/flynn/hardware.nix
@@ -91,7 +91,7 @@ in
 
     script = ''
       mkdir /btrfs_tmp
-      mount /dev/root_vg/root /btrfs_tmp
+      mount /dev/mapper/${luksDevice} /btrfs_tmp
       if [[ -e /btrfs_tmp/root ]]; then
           mkdir -p /btrfs_tmp/old_roots
           timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
