e8c74a065c17590c285ecf1a209e26ea4303f86a
Meow <github@xela.codes>
2026-06-11 15:12:18 -0400

    tf settings

--- nixos/flynn/timefinder.nix
+++ nixos/flynn/timefinder.nix
@@ -1,14 +1,22 @@
 {
   config,
   host,
+  hostname,
   pkgs,
+  xelib,
   ...
 }:
+let
+  app = xelib.apps."timefinder-electron-relay-${hostname}";
+in
 {
   environment.systemPackages = [
     (pkgs.timefinder-electron.override {
       extraEnv = {
         TIMEFINDER_BEARER_TOKEN_FILE = config.sops.secrets.timefinder-electron.path;
+        TIMEFINDER_SERVER_HOST = host.ip;
+        TIMEFINDER_SERVER_PORT = app.portString;
+        TIMEFINDER_WEBHOOK_BASE = app.url;
       };
     })
   ];
