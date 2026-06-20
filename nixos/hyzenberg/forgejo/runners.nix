f0eea2bb1eb143355d760fed07274322a5128703
Meow <github@xela.codes>
2026-06-20 03:04:12 -0400

    ffs

--- nixos/hyzenberg/forgejo/runners.nix
+++ nixos/hyzenberg/forgejo/runners.nix
@@ -6,33 +6,35 @@
   ...
 }:
 lib.mkMerge (
-  # map runners to instances
-  map
-    (runner: {
-      services.gitea-actions-runner.instances.${runner.name} = {
-        enable = true;
-        name = runner.name;
-        url = xelib.apps.forgejo.url;
-        tokenFile = config.sops.secrets."forgejo-runner-${runner.name}".path;
-      }
-      // runner.options;
+  (
+    # map runners to instances
+    map
+      (runner: {
+        services.gitea-actions-runner.instances.${runner.name} = {
+          enable = true;
+          name = runner.name;
+          url = xelib.apps.forgejo.url;
+          tokenFile = config.sops.secrets."forgejo-runner-${runner.name}".path;
+        }
+        // runner.options;
 
-      sops.envFiles."forgejo-runner-${runner.name}".TOKEN =
-        "op://Private/yjdttmakvgkuiia5xhda2pl3ve/Actions Runners/${runner.name}";
-    })
-    [
-      {
-        name = "xela-runner";
-        options = {
-          labels = [
-            "nix:docker://nixos/nix"
-          ];
-        };
-      }
-    ]
+        sops.envFiles."forgejo-runner-${runner.name}".TOKEN =
+          "op://Private/yjdttmakvgkuiia5xhda2pl3ve/Actions Runners/${runner.name}";
+      })
+      [
+        {
+          name = "xela-runner";
+          options = {
+            labels = [
+              "nix:docker://nixos/nix"
+            ];
+          };
+        }
+      ]
+  )
+  ++ [
+    {
+      services.gitea-actions-runner.package = pkgs.forgejo-runner;
+    }
+  ]
 )
-++ [
-  {
-    services.gitea-actions-runner.package = pkgs.forgejo-runner;
-  }
-]
