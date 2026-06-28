f98334448d74b10c5f4d17e7069784fd8349afa3
Meow <github@xela.codes>
2026-06-28 01:13:44 -0400

    move headless to nixos config

--- nixos/common/headless/default.nix
+++ nixos/common/headless/default.nix
@@ -1,9 +1,17 @@
 # any device without a gui
-_: {
+{ pkgs, ... }:
+{
   # just gets default systemd boot with a quick timeout
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
     timeout = 1;
   };
+
+  environment.systemPackages = with pkgs; [
+    # we can install the cli via nix for headless machines because it doesnt need desktop integration
+    _1password-cli
+    # for kitty ssh serverside support
+    kitty
+  ];
 }
