1bbf127e4f0e89c073032f6c5dd2c0ae27f0bc86
Meow <github@xela.codes>
2026-07-01 23:16:47 -0400

    kitty is alr included

--- nixos/_features/headless/default.nix
+++ nixos/_features/headless/default.nix
@@ -1,5 +1,5 @@
 # any device without a gui
-{ pkgs, ... }:
+{ ... }:
 {
   # just gets default systemd boot with a quick timeout
   boot.loader = {
@@ -7,9 +7,4 @@
     efi.canTouchEfiVariables = true;
     timeout = 1;
   };
-
-  environment.systemPackages = with pkgs; [
-    # for kitty ssh serverside support
-    kitty
-  ];
 }
