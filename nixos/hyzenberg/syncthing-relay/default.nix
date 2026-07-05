6f568f0feb9c4a284aedebe7fa51c1c0d2ecaa92
Meow <github@xela.codes>
2026-07-05 01:07:28 -0400

    basics

--- nixos/hyzenberg/syncthing-relay/default.nix
+++ nixos/hyzenberg/syncthing-relay/default.nix
@@ -1,4 +1,8 @@
-{ ... }: {
+{ config, ... }:
+let
+  app = config.apps.syncthing-relay;
+in
+{
   apps.syncthing-relay = {
     name = "syncthing-relay";
     port = 20089;
@@ -8,5 +12,36 @@
     };
   };
 
-  # "op://Private/hhigbwqelxxbtmnltlrwklilyy/cert"
+  services.syncthing = {
+    enable = true;
+    cert = config.sops.secrets.syncthing-relay-cert.path;
+    key = config.sops.secrets.syncthing-relay-key.path;
+
+    overrideDevices = true;
+    overrideFolders = true;
+
+    settings = {
+      options = {
+        # we dont need any of this
+        globalAnnounceEnabled = false;
+        localAnnounceEnabled = false;
+        relaysEnabled = false;
+        natEnabled = false;
+      };
+    };
+  };
+  systemd.services.syncthing.after = [ "tailscale-online.service" ];
+
+  sops.secrets.syncthing-relay-cert = {
+    sopsFile = config.sops.opSecrets.syncthing-relay.fullPath;
+    key = "cert";
+  };
+  sops.secrets.syncthing-relay-key = {
+    sopsFile = config.sops.opSecrets.syncthing-relay.fullPath;
+    key = "key";
+  };
+  sops.opSecrets.syncthing-relay.keys = {
+    cert = "op://Private/hhigbwqelxxbtmnltlrwklilyy/cert";
+    key = "op://Private/hhigbwqelxxbtmnltlrwklilyy/key";
+  };
 }
