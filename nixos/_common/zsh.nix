68933c302efd1acdcb9081bbbca997e1f33004c6
Meow <github@xela.codes>
2026-07-06 15:58:47 -0400

    atuin

--- nixos/_common/zsh.nix
+++ nixos/_common/zsh.nix
@@ -2,6 +2,7 @@
 {
   home-manager.importAll = [ ./zsh.hm.nix ];
 
+  programs.bash.enable = true;
   programs.zsh.enable = true;
 
   # set as default shell
@@ -10,4 +11,17 @@
 
   # persist the zsh_history directory
   persist.ed.home.userDirectories = [ ".local/share/zsh_history" ];
+  persist.ed.persist.directories = [ "/etc/atuin/data" ];
+
+  systemd.tmpfiles.rules = [ "d /etc/atuin/data 0755 xela xela - -" ];
+  programs.atuin = {
+    enable = true;
+    daemon.enable = true;
+    settings = {
+      #  auto_sync = true;
+      #  sync_frequency = "5m";
+      #  sync_address = "https://api.atuin.sh";
+      search_mode = "fuzzy";
+    };
+  };
 }
