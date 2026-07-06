d86a7d1d0b334108822450cd96884b7b49e86899
Meow <github@xela.codes>
2026-07-06 01:32:53 -0400

    hash

--- nixos/hyzenberg/flixur-demo/website.package.nix
+++ nixos/hyzenberg/flixur-demo/website.package.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation {
     owner = "xela";
     repo = "flixur-website";
     rev = "511a38b45e98c406ee2f65971240caf1d9e801de";
-    hash = "sha256-8CQwcgNvgeidHiCuN5BQg2bxH1xa5AVK1oxw5iN6FgQ=";
+    hash = "sha256-jEoL1TBlLJnIKQlQbelNZRZpJvSB3G55j0iBrDoV0Gs=";
   };
 
   # installPhase = ''
