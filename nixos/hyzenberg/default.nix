29136a9797c43477558b5759a63d0ec0413f6b8b
Meow <github@xela.codes>
2026-07-12 02:06:23 -0400

    fix v6 maybe

--- nixos/hyzenberg/default.nix
+++ nixos/hyzenberg/default.nix
@@ -1,10 +1,23 @@
 {
   config,
   host,
+  hostname,
+  xelib,
   ...
 }:
 {
   nginx.enable = true;
+
+  networking.defaultGateway6 = {
+    address = "fe80::1";
+    interface = "ens3";
+  };
+  networking.interfaces.ens3.ipv6.addresses = [
+    {
+      address = xelib.dns.addr6.${hostname};
+      prefixLength = 64;
+    }
+  ];
 
   sops.secrets.user_key = {
     sopsFile = config.sops.opSecrets.user_key.fullPath;
