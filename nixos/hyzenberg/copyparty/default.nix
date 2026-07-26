50d6faf043a63a5d8218a5cc5d956badbe08944e
Meow <github@xela.codes>
2026-07-26 13:31:31 -0400

    options

--- nixos/hyzenberg/copyparty/default.nix
+++ nixos/hyzenberg/copyparty/default.nix
@@ -62,8 +62,12 @@ in
       xff-src = "100.64.0.0/16"; # trust tailscale proxy
       og-ua = "(Discord|Twitter|Slack)bot"; # do opengraph for these user-agents only
 
+      # looks
+      ui-nocpla = true; # hide "connect" button
+
       # hardening
-      sss = true;
+      sss = true; # default safety
+      nid = true; # hide disk usage
       df = 10; # stop uploads <10gb free space
       ban-pw = "9,3600,86400"; # ban an IP for 24h after 9 bad password attempts in 1h
       ah-salt = "rGZhRXBn1W4DUaO+OIXvTB1A"; # salt for hashing, no way to get this from a sops secret iirc
