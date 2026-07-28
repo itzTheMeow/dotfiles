8e6065d7190c711fd44382a9de05848ac970672e
Meow <github@xela.codes>
2026-07-28 01:28:47 -0400

    update unstable

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # dotfiles-nix
 
 A lot of stuff was inspired from [AlexNabokikh's Nix Config](https://github.com/AlexNabokikh/nix-config).
+
+### "TODO:" Notes
+
+- `TODO:pr` - Waiting on a PR to merge.
+- `TODO:26.11` - Waiting on next nixpkgs release.
--- nixos/_features/vscode/default.nix
+++ nixos/_features/vscode/default.nix
@@ -9,5 +9,20 @@
   };
 
   # temporary
-  environment.systemPackages = [ pkgs-unstable.kilo ];
+  environment.systemPackages = [
+    #TODO:pr https://github.com/NixOS/nixpkgs/issues/542516
+    #TODO:26.11 use actual package??
+    (pkgs-unstable.kilo.overrideAttrs (old: {
+      nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs-unstable.nodejs_22 ];
+      postPatch = (old.postPatch or "") + ''
+        substituteInPlace packages/opencode/script/build.ts \
+          --replace-fail '      console.log(`Running smoke test: ''${binaryPath} --pure models anthropic`)' \
+                         '      console.log("Skipping models snapshot smoke test")' \
+          --replace-fail '      await smokeModels(binaryPath)' \
+                         '      await Promise.resolve()' \
+          --replace-fail '      console.log("Models snapshot smoke test passed")' \
+                         '      void 0'
+      '';
+    }))
+  ];
 }
