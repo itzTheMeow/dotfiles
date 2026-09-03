b12dc938da33fc627e0ff1f216d071711c60068b
Meow <github@xela.codes>
2026-09-03 18:32:10 -0400

    remove opencode-desktop

--- nixos/_features/opencode/default.nix
+++ nixos/_features/opencode/default.nix
@@ -1,9 +1,6 @@
 { pkgs-unstable, ... }: {
   #TODO:26.11 swap to regular
-  environment.systemPackages = with pkgs-unstable; [
-    opencode
-    opencode-desktop
-  ];
+  environment.systemPackages = with pkgs-unstable; [ opencode ];
 
   persist.ed.home.userDirectories = [
     ".config/opencode"
