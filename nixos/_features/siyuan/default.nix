b847b777d1760c2bd4746e18a99384cbcca219f4
Meow <github@xela.codes>
2026-09-12 02:52:18 -0400

    siyuan dirs

--- nixos/_features/siyuan/default.nix
+++ nixos/_features/siyuan/default.nix
@@ -1,17 +1,9 @@
 { pkgs, ... }: {
   environment.systemPackages = [ pkgs.siyuan ];
 
-  # Everything SiYuan writes outside the workspace lives in the home dir:
-  #   ~/Siyuan                       the workspace itself (data/ + conf/)
-  #   ~/.siyuan                      app metadata (kernel ~/.siyuan/conf.json)
-  #   ~/.config/siyuan               workspace selection / electron user data
-  #   ~/.config/siyuan.bak           legacy workspace location record
-  #   ~/.config/SiYuan-Electron      legacy electron user data
   persist.ed.home.userDirectories = [
-    "Siyuan"
-    ".siyuan"
-    ".config/siyuan"
-    ".config/siyuan.bak"
-    ".config/SiYuan-Electron"
+    ".config/siyuan" # app settings
+    ".config/SiYuan-Electron" # electron data
+    ".local/share/siyuan-data" # actual workspace data
   ];
 }
