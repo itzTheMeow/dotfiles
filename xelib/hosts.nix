3ad53ab51821a9c030567c785ba979d512e44feb
Meow <github@xela.codes>
2026-09-22 03:07:51 -0400

    fix ip

--- xelib/hosts.nix
+++ xelib/hosts.nix
@@ -147,6 +147,7 @@ in
     };
     ipad4 = {
       username = "mobile";
+      ip = "100.64.0.0";
       ports.ssh = 22;
       publicKey = "op://Private/ufo2p565cqdvpfzf574k4k23hy/public key";
     };
